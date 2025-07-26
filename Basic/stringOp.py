b72677e968c0af64ff4ca071221508a854625a33
Pratick Baraik <animeNerd.int@protonmail.com>
2025-07-26 09:37:14 +0530

    adding string operation file of python

---
+++ Basic/stringOp.py
@@ -0,0 +1,11 @@
+str_1 = "I am a python developer. "
+str_2 = "You are noob. "
+
+#printing str_1 ten times
+print(str_1 * 10)
+
+#printing str_2 five times
+print(str_2 * 5)
+
+# adding str_1 and str_2
+print(str_1 + str_2)
