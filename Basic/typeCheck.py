1fa861cc241105650ae2daefb8485a4753f5461f
Pratick Baraik <animeNerd.int@protonmail.com>
2025-07-26 09:11:00 +0530

    type checking all the supported data types of python

---
+++ Basic/typeCheck.py
@@ -0,0 +1,60 @@
+#checking the data types here
+val_int = 100
+print(f'val_int = {val_int}')
+print(type(val_int))
+
+val_float = 33.89
+print(f'val_float = {val_float}')
+print(type(val_float))
+
+val_str = "I am a string"
+print(f'val_str = {val_str}')
+print(type(val_str))
+
+val_bool = False
+print(f'val_bool = {val_bool}')
+print(type(val_bool))
+
+val_complex = complex(13, 67)
+print(f'val_complex = {val_complex}')
+print(type(val_complex))
+
+val_list = [2, 3, 4, 5]
+print(f'val_list = {val_list}')
+print(type(val_list))
+
+val_tuple = (1.34, 2.45, 78.67, 34.90)
+print(f'val_tuple = {val_tuple}')
+print(type(val_tuple))
+
+val_range = range(1, 5)
+print(f'val_range = {val_range}')
+print(type(val_range))
+
+val_map = {'a': 1, 'b': 2, 'c': 3}
+print(f'val_map = {val_map}')
+print(type(val_map))
+
+val_set = {1, 2, 3, 4, 5}
+print(f'val_set = {val_set}')
+print(type(val_set))
+
+val_frozenset = frozenset([1, 2, 3, 4, 5, 6])
+print(f'val_frozenset = {val_frozenset}')
+print(type(val_frozenset))
+
+val_bytes = b'hi'
+print(f'val_bytes = {val_bytes}')
+print(type(val_bytes))
+
+val_bytearray = bytearray([10, 20, 30, 40])
+print(f'val_bytearray = {val_bytearray}')
+print(type(val_bytearray))
+
+val_memoryview = memoryview(b'abc')
+print(f'val_memoryview = {val_memoryview}')
+print(type(val_memoryview))
+
+val_none = None
+print(f'val_none = {val_none}')
+print(type(val_none))
