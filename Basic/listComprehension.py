f2e7383e6e468300bbb03336b3f7c3c9e6d6ef0d
Pratick Baraik <animeNerd.int@protonmail.com>
2025-07-28 12:51:59 +0530

    pushing listComprehension python file which includes practical examples

---
+++ Basic/listComprehension.py
@@ -0,0 +1,33 @@
+# creating a list of squares till 30
+squares = [x ** 2 for x in range(1, 31)]
+print("squares till 30:", squares)
+print()
+
+# creating a list of cubes till 30
+cubes = [x ** 3 for x in range(1, 31)]
+print("cubes till 30:", cubes)
+print()
+
+# creating a list of even numbers till 20
+evens = [x for x in range(1, 21) if x % 2 == 0]
+print("even numbers till 20:", evens)
+print()
+
+# creating a list of odd numbers till 20
+odds = [x for x in range(1, 21) if x % 2 != 0]
+print("odd numbers till 20:", odds)
+print()
+
+# converting strings to uppercase
+words = ['hello', 'world', 'python', 'coder']
+upper_words = [abc.upper() for abc in words]
+print("Uppercase words:", upper_words)
+lower_words = [lower.lower() for lower in upper_words]
+print("Lowercase words:", lower_words)
+print()
+
+# nested list comprehension example
+matrix = [[1, 2], [3, 4], [5, 6]]
+flattened = [num for row in matrix for num in row]
+print("flattened matrix:", flattened)
+print()
